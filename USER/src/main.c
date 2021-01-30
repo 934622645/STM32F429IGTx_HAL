b1dee6989f7730a0c6f855f2eaeb73d2ac30fb82
haoge <934622645@qq.com>
2021-01-30 20:07:15 +0800

    2021-1-30

---
+++ USER/src/main.c
@@ -0,0 +1,13 @@
+/*
+ * @Author: haoge2000
+ * @Date: 2021-01-30 19:12:29
+ * @LastEditors: haoge2000
+ * @LastEditTime: 2021-01-30 19:57:20
+ * @FilePath: \first_test_project\USER\src\main.c
+ */
+#include "main.h"
+
+int main(void)
+{
+  return 0;
+}
